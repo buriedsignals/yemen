3d9a102f613dc213a44985b39919a249f206c64f
buriedsignals <tom.vaillant@icloud.com>
2022-07-28 20:13:17 +0200

    prepqre for production

--- src/components/Credit/Credit.jsx
+++ src/components/Credit/Credit.jsx
@@ -34,6 +34,8 @@ export default function Credit({ animate = false }) {
               <li>Remy Dumas</li>
               <li>Hassan Saffiedine</li>
               <li>Zeinab Saffiedine</li>
+              <li>Arwa Mokdad</li>
+              <li>Aishaa Jumaan</li>
               <br />
               <li>WITH DATA FROM:</li>
               <li>ACLED</li>
